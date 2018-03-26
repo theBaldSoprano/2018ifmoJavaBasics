26a7a515a185c8c2999a2f8677b7896ae56161a1
Gregory <parentalbox@gmail.com>
2018-03-26 22:16:07 +0300

    solved lesson1.task8 getMostFrequentlyAppeared element

--- src/com/study/itmo/gregory/lesson1/Solution.java
+++ src/com/study/itmo/gregory/lesson1/Solution.java
@@ -2,18 +2,22 @@ package com.study.itmo.gregory.lesson1;
 
 import java.io.IOException;
 
+import static com.study.itmo.gregory.lesson1.Task8.getMostFrequentlyAppeared;
+
 
 public class Solution {
 
     public static void main(String[] args) throws IOException {
 
-        int[] array = {5, 66, 6, 3333, 5, 3};
+        int[] array = {5, 66, 6, 3333, 5, 3, 5, 5, 3333, 3333, 66};
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        int n = 2;
+
+        int[] result = getMostFrequentlyAppeared(array, n);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
         }
-
-        System.out.println("+++++++++++++++++++++++");
 
 
 
---
+++ src/com/study/itmo/gregory/lesson1/Task8.java
@@ -0,0 +1,53 @@
+package com.study.itmo.gregory.lesson1;
+
+/*Дан массив и число K. Найдите первые K самых часто встречающихся
+элементов.*/
+
+import java.util.*;
+
+public class Task8 {
+
+    public static int[] getMostFrequentlyAppeared(int[] array, int n) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++) {
+
+            int current = array[i];
+            int matches = -1;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == current) matches++;
+            }
+            map.put(current, matches);
+
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            public int compare(Map.Entry<Integer, Integer> o1,
+                               Map.Entry<Integer, Integer> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        Map<Integer, Integer> sortedMap = new LinkedHashMap<Integer, Integer>();
+        for (Map.Entry<Integer, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        int[] result = new int[n];
+        int count = 0;
+
+        Iterator<Map.Entry<Integer, Integer>> iterator = sortedMap.entrySet().iterator();
+
+        for (; count < n; count++) {
+            result[count] = iterator.next().getKey();
+        }
+
+        return result;
+
+
+    }
+
+}
