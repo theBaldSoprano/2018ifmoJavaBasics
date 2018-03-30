e9ea09f2caf59137b72d543454aa8c7d66ea6881
Gregory <parentalbox@gmail.com>
2018-03-30 23:18:33 +0300

    added append and constructor for MyStringBuilder

--- src/com/study/itmo/gregory/lesson5/mystringbuilderTask3/MyStringBuilder.java
+++ src/com/study/itmo/gregory/lesson5/mystringbuilderTask3/MyStringBuilder.java
@@ -4,12 +4,25 @@ public class MyStringBuilder {
 
     /*todo создать свой класс МойСтрингБилдер
 у него будет свой метод аппэнд
-внутри которого будет сначала выполняться аппенд делигируемый стрингу
+внутри которого будет сначала выполняться аппенд делигируемый стрингбилдеру
 а потом в очередь (линкедлист) будет добавляться действие обратное аппенду
 и потом выполняя отмену действия будет выполняться
  */
 
-    StringBuilder stringBuilder = new StringBuilder();
+    private StringBuilder stringBuilder;
 
+    public MyStringBuilder(String str){
+        stringBuilder = new StringBuilder(str);
+    }
 
+    public MyStringBuilder append(String str) {
+        stringBuilder.append(str);
+
+        //todo тут дописывай свое действие
+        //так можно кастомайзить поведения если не заовверрайдить
+        //стрингбилдер (выше поле) хранит стринг с которым мы работаем
+        //далее просто вызови его туСтринг
+
+        return this;
+    }
 }
---
+++ src/com/study/itmo/gregory/lesson5/mystringbuilderTask3/Solution.java
@@ -0,0 +1,9 @@
+package com.study.itmo.gregory.lesson5.mystringbuilderTask3;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        StringBuilder builder = new StringBuilder("foo");
+
+    }
+}
